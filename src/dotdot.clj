beacf85d421057739f4dc373a3db517b1d0e14d7
David Nolen <david.nolen@gmail.com>
2015-06-20 15:22:55 -0400

    debugging .. issue

---
+++ src/dotdot.clj
@@ -0,0 +1,22 @@
+(ns dotdot
+  (:require [clojure.java.io :as io]
+            [cljs.env :as env]
+            [cljs.analyzer :as ana]
+            [cljs.compiler :as comp]
+            [cljs.closure :as closure]))
+
+(def cenv (env/default-compiler-env))
+
+(comment
+  (env/with-compiler-env cenv
+    (let [src (io/resource "cljs/core.cljc")]
+      (closure/compile src
+        {:output-file (closure/src-file->target-file src)
+         :force       true
+         :mode        :interactive})))
+
+  (env/with-compiler-env cenv
+    (-> (ana/analyze {:ns {:name 'cljs.core$macros}}
+          'cljs.core$macros/..)
+      :info comp/munge))
+  )
