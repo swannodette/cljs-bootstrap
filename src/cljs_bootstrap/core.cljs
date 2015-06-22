ae5cb21910b3abf4ab2f88434cc6c0d25f889bf4
David Nolen <david.nolen@gmail.com>
2015-06-22 08:24:37 -0400

    wip

--- src/cljs_bootstrap/core.cljs
+++ src/cljs_bootstrap/core.cljs
@@ -1,11 +1,16 @@
 (ns cljs-bootstrap.core
   (:require-macros [cljs.env.macros :refer [ensure]])
-  (:require [cljs.tools.reader :as r]
+  (:require [cljs.pprint :refer [pprint]]
+            [cljs.tools.reader :as r]
             [cljs.tools.reader.reader-types :refer [string-push-back-reader]]
             [cljs.analyzer :as ana]
             [cljs.compiler :as c]))
 
+(set! *target* "nodejs")
+
 (comment
+  (apply load-file ["./.cljs_node_repl/cljs/core$macros.js"])
+
   (js/eval
     (with-out-str
       (c/emit
@@ -13,6 +18,48 @@
           (ana/analyze-keyword
             (assoc (ana/empty-env) :context :expr)
             :foo)))))
+
+  (js/eval
+    (with-out-str
+      (c/emit
+        (ensure
+          (ana/analyze
+            (assoc (ana/empty-env) :context :expr)
+            '(+ 1 2))))))
+
+  ;; fails
+  (ensure
+    (ana/get-expander
+      (first '(first [1 2 3]))
+      (assoc (ana/empty-env) :context :expr)))
+
+  ;; fails
+  (pprint
+    (ensure
+      (ana/macroexpand-1
+        (assoc (ana/empty-env) :context :expr)
+        '(first [1 2 3]))))
+
+  ;; fails
+  (pprint
+    (ensure
+      (ana/analyze-seq
+        (assoc (ana/empty-env) :context :expr)
+        '(first [1 2 3]) nil nil)))
+
+  (js/eval
+    (with-out-str
+      (c/emit
+        (ensure
+          (ana/analyze
+            (assoc (ana/empty-env) :context :expr)
+            '(first [1 2 3]))))))
+
+  (pprint
+    (ensure
+      (ana/analyze
+        (assoc (ana/empty-env) :context :expr)
+        '(fn [a b] (+ a b)))))
 
   ;; ignore for now
 
