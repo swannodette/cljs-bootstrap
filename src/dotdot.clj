c2289ab6a14dee0388622b770e7a1d644081e3d2
David Nolen <david.nolen@gmail.com>
2015-07-06 17:28:37 -0400

    clojure.tools.reader perf comparison

--- src/dotdot.clj
+++ src/dotdot.clj
@@ -3,7 +3,10 @@
             [cljs.env :as env]
             [cljs.analyzer :as ana]
             [cljs.compiler :as comp]
-            [cljs.closure :as closure]))
+            [cljs.closure :as closure]
+            [cljs.tagged-literals :as tags]
+            [clojure.tools.reader :as r]
+            [clojure.tools.reader.reader-types :refer [string-push-back-reader]]))
 
 (def cenv (env/default-compiler-env))
 
@@ -19,4 +22,20 @@
     (comp/munge
       (ana/resolve-var {:ns {:name 'cljs.core$macros}}
         'cljs.core$macros/..)))
+
+  (def f (slurp (io/resource "cljs/core.cljs")))
+
+  (string? f)
+
+  ;; 1.3s on work machine
+  (time
+    (dotimes [_ 10]
+      (let [rdr (string-push-back-reader f)
+            eof (Object.)]
+        (binding [*ns* (create-ns 'cljs.analyzer)
+                  r/*data-readers* tags/*cljs-data-readers*]
+          (loop []
+            (let [x (r/read {:eof eof} rdr)]
+              (when-not (identical? eof x)
+                (recur))))))))
   )
