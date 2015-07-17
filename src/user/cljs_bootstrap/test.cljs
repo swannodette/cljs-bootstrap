0116cb15981e5501ef5124504ace5f40301c498d
dnolen <dnolen@cognitect.com>
2015-07-17 19:47:06 -0400

    latest

--- src/user/cljs_bootstrap/test.cljs
+++ src/user/cljs_bootstrap/test.cljs
@@ -6,6 +6,11 @@
 
 (enable-console-print!)
 
+(set! *target* "nodejs")
+(def cenv (cljs/empty-env))
+
 (comment
-  cljs/eval
+  (cljs/eval cenv '(defn foo [a b] (+ a b))
+    (fn [res]
+      (println res)))
   )
