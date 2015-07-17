d0f118a56d412c1dcb182b60d20d02bcb1890ab7
dnolen <dnolen@cognitect.com>
2015-07-17 18:45:07 -0400

    wow can load cljs.js in a Node REPL, find-macros-ns confirms the macro ns is available

--- src/user/cljs_bootstrap/test.cljs
+++ src/user/cljs_bootstrap/test.cljs
@@ -1,4 +1,11 @@
+;; IMPORTANT: to load this file in a REPL or compile it you must first
+;; delete src/clojure/cljs/core$macros.js
+
 (ns cljs-bootstrap.test
   (:require [cljs.js :as cljs]))
 
 (enable-console-print!)
+
+(comment
+  cljs/eval
+  )
