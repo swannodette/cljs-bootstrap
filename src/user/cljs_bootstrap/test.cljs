681da59a983dc449f0bb704e6a5603d1cc24e3e8
David Nolen <david.nolen@gmail.com>
2015-07-17 22:11:38 -0400

    wip

--- src/user/cljs_bootstrap/test.cljs
+++ src/user/cljs_bootstrap/test.cljs
@@ -4,6 +4,8 @@
 (ns cljs-bootstrap.test
   (:require-macros [cljs.env.macros :as env])
   (:require [cljs.js :as cljs]
+            [cljs.pprint :as pp]
+            [cljs.env :as env]
             [cljs.analyzer :as ana]
             [cljs.compiler :as comp]))
 
@@ -13,11 +15,30 @@
 (def cenv (cljs/empty-env))
 
 (comment
-  ;; ns missing problem
-  (env/with-compiler-env cenv
+  (pp/pprint
+    (binding [env/*compiler* cenv
+              ana/*cljs-ns* 'cljs.user]
+      (ana/get-namespace ana/*cljs-ns*)))
+
+  (pp/pprint
+    (binding [env/*compiler* cenv
+              ana/*cljs-ns* 'cljs.user]
+      (assoc (ana/empty-env)
+        :ns (ana/get-namespace ana/*cljs-ns*))))
+
+  (binding [env/*compiler* cenv
+            ana/*cljs-ns* 'cljs.user]
+    (assoc (ana/empty-env)
+      :ns {:name ana/*cljs-ns*}))
+
+  ;; works
+  ;; cljs.user.foo = ...
+  (binding [env/*compiler* cenv
+            ana/*cljs-ns*  'cljs.user]
     (with-out-str
       (comp/emit
-        (ana/analyze (ana/empty-env)
+        (ana/analyze (assoc (ana/empty-env)
+                       :ns (ana/get-namespace ana/*cljs-ns*))
           '(defn foo [a b] (+ a b))))))
 
   ;; does not work yet
