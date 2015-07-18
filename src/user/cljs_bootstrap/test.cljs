d53b6ff04f34b2abf557fcf9c50ff760a2829d70
David Nolen <david.nolen@gmail.com>
2015-07-18 12:20:08 -0400

    working now

--- src/user/cljs_bootstrap/test.cljs
+++ src/user/cljs_bootstrap/test.cljs
@@ -15,33 +15,7 @@
 (def cenv (cljs/empty-env))
 
 (comment
-  (pp/pprint
-    (binding [env/*compiler* cenv
-              ana/*cljs-ns* 'cljs.user]
-      (ana/get-namespace ana/*cljs-ns*)))
-
-  (pp/pprint
-    (binding [env/*compiler* cenv
-              ana/*cljs-ns* 'cljs.user]
-      (assoc (ana/empty-env)
-        :ns (ana/get-namespace ana/*cljs-ns*))))
-
-  (binding [env/*compiler* cenv
-            ana/*cljs-ns* 'cljs.user]
-    (assoc (ana/empty-env)
-      :ns {:name ana/*cljs-ns*}))
-
   ;; works
-  ;; cljs.user.foo = ...
-  (binding [env/*compiler* cenv
-            ana/*cljs-ns*  'cljs.user]
-    (with-out-str
-      (comp/emit
-        (ana/analyze (assoc (ana/empty-env)
-                       :ns (ana/get-namespace ana/*cljs-ns*))
-          '(defn foo [a b] (+ a b))))))
-
-  ;; does not work yet
   (cljs/eval cenv '(defn foo [a b] (+ a b))
     (fn [res]
       (println res)))
