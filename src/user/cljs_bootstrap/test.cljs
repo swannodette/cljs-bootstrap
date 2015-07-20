f319e4ac4839868c3c74e5603156e35399e3e5f6
David Nolen <david.nolen@gmail.com>
2015-07-20 09:39:13 -0400

    latest state

--- src/user/cljs_bootstrap/test.cljs
+++ src/user/cljs_bootstrap/test.cljs
@@ -1,5 +1,6 @@
 (ns cljs-bootstrap.test
-  (:require [cljs.js :as cljs]))
+  (:require [cljs.js :as cljs]
+            [cljs.analyzer :as ana]))
 
 (enable-console-print!)
 
@@ -7,8 +8,28 @@
 (def cenv (cljs/empty-env))
 
 (comment
+  (require-macros '[cljs.env.macros :as env])
+  (require '[cljs.pprint :as pp]
+           '[cljs.env :as env]
+           '[cljs.analyzer :as ana])
+
   ;; works
   (cljs/eval cenv '(defn foo [a b] (+ a b))
+    (fn [res]
+      (println res)))
+
+  ;; 2nd eval fails
+  (cljs/eval cenv '(defn bar [a b] (+ a b))
+    (fn [res]
+      (println res)))
+
+  (cljs/compile cenv "(defn foo [a b] (+ a b))"
+    (fn [js-source]
+      (println js-source)))
+
+  (cljs/eval-str cenv
+    "(defn foo [a b] (+ a b))
+     (defn bar [c d] (+ c d))"
     (fn [res]
       (println res)))
   )
